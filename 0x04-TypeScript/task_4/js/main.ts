58171c2c8a6eb63d980f237299676120efcb4927
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-14 01:16:27 +0000

    `feat: create and export subjects and teacher, log requirements and availability`

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,35 +1,34 @@
-/// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/Java.ts" />
-/// <reference path="./subjects/React.ts" />
+// task_4/js/main.ts
 
-import { Subjects } from "./Teacher";
+import { Cpp, Java, React } from './subjects';
+import { Teacher } from './teacher';
 
 // Create and export constants for each subject
-export const cpp = new Subjects.Cpp();
-export const java = new Subjects.Java();
-export const react = new Subjects.React();
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
 // Create and export a Teacher object
-export const cTeacher: Subjects.Teacher = {
-  firstName: "John",
-  lastName: "Doe",
-  experienceTeachingC: 10,
+export const cTeacher: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
 };
 
-// Cpp subject
-console.log("C++");
-cpp.setTeacher(cTeacher);
+// Cpp Subject
+console.log('C++');
+cpp.teacher = cTeacher;
 console.log(cpp.getRequirements());
 console.log(cpp.getAvailableTeacher());
 
-// Java subject
-console.log("Java");
-java.setTeacher(cTeacher);
+// Java Subject
+console.log('Java');
+java.teacher = cTeacher;
 console.log(java.getRequirements());
 console.log(java.getAvailableTeacher());
 
-// React subject
-console.log("React");
-react.setTeacher(cTeacher);
+// React Subject
+console.log('React');
+react.teacher = cTeacher;
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
