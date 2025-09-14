6e5c73611aff6526cd8ba491d12c379e50fa4453
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-14 01:26:37 +0000

    `fix: add implementations for sumMajorCredits and sumMinorCredits`

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -9,3 +9,25 @@ export interface MinorCredits {
   credits: number;
   readonly brand: 'MinorCredits';
 }
+
+// Function to sum MajorCredits
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MajorCredits',
+  };
+}
+
+// Function to sum MinorCredits
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MinorCredits',
+  };
+}
