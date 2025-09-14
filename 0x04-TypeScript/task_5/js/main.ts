ff431d7a77bcaf058d9b67d47ca114762578c3ea
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-14 01:25:33 +0000

    `feat: add MajorCredits and MinorCredits interfaces with sum functions`

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,11 @@
+// MajorCredits interface
+export interface MajorCredits {
+  credits: number;
+  readonly brand: 'MajorCredits';
+}
+
+// MinorCredits interface
+export interface MinorCredits {
+  credits: number;
+  readonly brand: 'MinorCredits';
+}
