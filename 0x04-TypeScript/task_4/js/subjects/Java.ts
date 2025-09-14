8d77e9616895b572fe1578244165117dd131935b
ebenamoafo <ebenamoafo222@gmail.com>
2025-09-14 01:02:38 +0000

    feat: add Java subject class with teacher experience check in Subjects namespace

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,21 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Extend Teacher interface with new optional property
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Java";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return "No available teacher";
+    }
+  }
+}
---
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -0,0 +1,21 @@
+/// <reference path="./Subject.ts" />
+
+namespace Subjects {
+  // Extend Teacher interface with new optional property
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for React";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return "No available teacher";
+    }
+  }
+}
